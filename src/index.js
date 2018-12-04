3af1940a123396af89389e76862329069b5a53fb
NadiaAiraf <aidanfaria@gmail.com>
2018-12-04 14:16:31 +0000

    starting off game of life creation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+  return (
+    <button className={props.color} onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blackSquares: Array(100).fill(false),
+    }
+  }
+
+  color(square) {
+    if (square) {
+      return 'square-black'
+    } else {
+      return 'square-white'
+    }
+  }
+
+  changeColor (i) {
+    console.log(i)
+      const current = this.state.blackSquares;
+      current[i] = !current[i];
+      this.setState({
+        blackSquares: current
+      })
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        onClick={() => this.changeColor(i)}
+        color={this.color(this.state.blackSquares[i])}
+        value={i}
+      />
+    );
+  }
+
+  renderTable() {
+    let table = []
+    for (var j = 0; j < 10; j++) {
+      let row = []
+      for (var i = 0; i < 10; i++) {
+        row.push(this.renderSquare(i+j*10))
+      }
+      table.push(<div className="board-row">{row}</div>)
+    }
+    return table
+  }
+
+  render() {
+    return(
+      <div>
+        {this.renderTable()}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
